05db0374aa0bfc4c49640959dc0adf13bba85411
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-09-03 07:48:10 +0000

    Reset file
    File: components/simple_cta_component_1725349462.js
    Git Ref: bc5af7667d955619023b52c1201d5102735571b8
    Prune: False

--- components/simple_cta_component_1725349462.js
+++ components/simple_cta_component_1725349462.js
@@ -20,10 +20,6 @@ INSTRUCTION: Summary: This component contains a h2 heading title for the CTA, a 
                 <div id="cta-button-container" class="flex">
                     <a id="cta-button" href="#" class="flex-1 text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">
                         Start Creating Now
-                        Start Creating Now
-                    </a>
-                    <a id="ansh-button" href="#" class="flex-1 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 ml-2 mb-2 dark:bg-blue-500 dark:hover:bg-blue-600 focus:outline-none dark:focus:ring-blue-800">
-                        Ansh
                     </a>
                 </div>
             </div>
